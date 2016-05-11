b07462a992dd04e3122262cf423d454afdf60107
carloserodriguez2000 <carloserodriguez2000@yahoo.com>
2016-05-11 18:46:11 -0500

    added while loop to continue recalcualting at users choice

--- MyPay.py
+++ MyPay.py
@@ -1,13 +1,31 @@
 
 # hi. This is a little program
+overTimeRate = 1.5                                              # Set overtime rate to 1.5/ Time and a half.
+continueLoop = "1"                                                # initialize loop flag to 1. continue loping.
 
-hourlyRate = input("Please enter you Hourly rate+ ")  		# Get user input. the hourly rate.
-hourlyRate = float(hourlyRate)							# convert to a number just in case.
+while continueLoop == "1" :
+    hourlyRate = input("Please enter you Hourly rate= ")  		# Get user input. the hourly rate.
+    hourlyRate = float(hourlyRate)					# convert to a number just in case.
 
-hoursWorked = input("Please enter total number of hours= ")	# get user input. number of hours
-hoursWorked = float(hoursWorked)							# convert to a number just in case.
+    hoursWorked = input("Please enter total number of hours= ")	# get user input. number of hours
+    hoursWorked = float(hoursWorked)				# convert to a number just in case.
 
-totalPay = hoursWorked * hourlyRate;
+    if( hoursWorked >40):
+        overTimeHours = hoursWorked - 40
+        hoursWorked = 40
+    else:
+        overTimeHours = 0
 
-print("Total Pay = ", totalPay)
+    # print(overTimeHours)                                      # helper function to find bug in calculations. not needed in run time.
+    # print(overTimeHours * hourlyRate * overTimeRate)          # helper function to find bug in calculations. not needed in run time.
+        
+    totalPay = (hoursWorked * hourlyRate) + (overTimeHours * hourlyRate * overTimeRate)
+
+    print("Total Pay = ", totalPay)
+
+    continueLoop = input("Press 1 to run again")
+    print(continueLoop)
+else :
+    print("Thank you .  Bye.")
+    
 
